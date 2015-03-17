4d0346e268cfc403b66d5668c12bc77452560ba3
Teddy Chan <teddychan@gmail.com>
2015-03-18 00:52:19 +0800

    update example

--- README.md
+++ README.md
@@ -10,27 +10,34 @@ _(Coming soon)_
 
 ## Examples
 ```
-var Rlimiter = require('../lib/index');
+var Rlimiter = require('rlimiter');
 
 var redis = require('redis');
-var redis_client = redis.createClient();
+var redis_client = redis.createClient(6379, 'localhost');
+
 redis_client.on('connect', function(err) {
-	redis_client.select(3, function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		redis_client.select(3, function(err) {
+			if (err) {
+				console.log(err);
+			} else {
+				// limit to 2 request per every 10s
+				var rlimiter = new Rlimiter({
+					redis_client: redis_client,
+					key: 'the-user-api-key',
+					rate_limit: 2,
+					duration: 10
+				});
 
-		var rlimiter = new Rlimiter({
-			redis_client: redis_client,
-			key: 'usps',
-			rate_limit: 2,
-			duration: 10
+				rlimiter.get(function(err, results) {
+					console.log(err);
+					console.log(results);
+				});
+			}
 		});
-
-		rlimiter.get(function(err, results) {
-
-			console.log(err);
-			console.log(results);
-		});
-
-	});
+	}
 });
 
 ```
--- example/example.js
+++ example/example.js
@@ -1,7 +1,8 @@
-var Rlimiter = require('..');
+var Rlimiter = require('../lib/');
 
 var redis = require('redis');
-var redis_client = redis.createClient();
+var redis_client = redis.createClient(6379, 'localhost');
+
 redis_client.on('connect', function(err) {
 	if (err) {
 		console.log(err);
@@ -10,37 +11,14 @@ redis_client.on('connect', function(err) {
 			if (err) {
 				console.log(err);
 			} else {
+				// limit to 2 request per every 10s
 				var rlimiter = new Rlimiter({
 					redis_client: redis_client,
-					key: 'usps',
+					key: 'the-user-api-key',
 					rate_limit: 2,
 					duration: 10
 				});
 
-				rlimiter.get(function(err, results) {
-					console.log(err);
-					console.log(results);
-				});
-				rlimiter.get(function(err, results) {
-					console.log(err);
-					console.log(results);
-				});
-				rlimiter.get(function(err, results) {
-					console.log(err);
-					console.log(results);
-				});
-				rlimiter.get(function(err, results) {
-					console.log(err);
-					console.log(results);
-				});
-				rlimiter.get(function(err, results) {
-					console.log(err);
-					console.log(results);
-				});
-				rlimiter.get(function(err, results) {
-					console.log(err);
-					console.log(results);
-				});
 				rlimiter.get(function(err, results) {
 					console.log(err);
 					console.log(results);
